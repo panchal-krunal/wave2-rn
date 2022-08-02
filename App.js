81c07b266f455dbbb284486c524e22795296e4d4
Krunal Panchal <knpddit@gmail.com>
2022-08-02 11:18:07 +0530

    changes for socket

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar, SafeAreaView} from 'react-native';
 import Navigation from './src/navigation';
 import {PersistGate} from 'redux-persist/integration/react';
@@ -7,6 +7,30 @@ import configureStore from './src/redux/store';
 const {store, persistor} = configureStore();
 
 const App = () => {
+  const initializeSocket = () => {
+    const ws = new WebSocket('wss://app2.fisdom.com:4515');
+
+    ws.onopen = () => {
+      console.log('socket opened');
+      // connection opened
+      ws.send(
+        '63=FT3.0|64=101|65=74|66=14:59:22|67=TEST|68=YOUR_API_KEY|4=|400=0|401=2|396=HO|51=4|395=127.0.0.1',
+      ); // send a message
+    };
+
+    ws.onmessage = e => {
+      // a message was received
+      console.log(e.data);
+    };
+
+    ws.onerror = e => {
+      // an error occurred
+      console.log(e.message);
+    };
+  };
+  useEffect(() => {
+    initializeSocket();
+  }, []);
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
